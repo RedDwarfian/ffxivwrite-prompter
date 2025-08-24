bb7647c87caf275cf33ee71390732b7a4243d7be
RedDwarfian <reddwarf86esb@gmail.com>
2025-08-24 16:31:47 -0400

    Added spin animation.

--- src/app/component/writing-prompt-generator/writing-prompt-generator.component.ts
+++ src/app/component/writing-prompt-generator/writing-prompt-generator.component.ts
@@ -12,6 +12,13 @@ import { promptData } from '../../../environment/environment';
 export class WritingPromptGeneratorComponent {
   public spinOptions = inject(SpinOptionsService);
   public randomPrompt: promptInterface | null = null;
+  public displayPrompts: promptInterface[] = [];
+
+  // We want to reset the animation every time we spin.
+  private rollerKey = 0;
+  get rollKey() {
+    return this.rollerKey++;
+  }
 
   public spin() {
     console.log('Spinning with options:', this.spinOptions.mode(), this.spinOptions.specifiedYear(), this.spinOptions.specifiedDate(), this.spinOptions.includeFree());
@@ -33,12 +40,20 @@ export class WritingPromptGeneratorComponent {
       }
       return true;
     });
+    this.randomPrompt = null;
+    this.displayPrompts = [];
     console.log('Culled prompts:', culledPrompts);
     if (culledPrompts.length > 0) {
-      const randomIndex = Math.floor(Math.random() * culledPrompts.length);
+      let randomIndex = Math.floor(Math.random() * culledPrompts.length);
       this.randomPrompt = culledPrompts[randomIndex];
-    } else {
-      this.randomPrompt = null;
+
+      // Add the prompts to the displayPrompts so we can animate them all flying by.
+      while (this.displayPrompts.unshift(structuredClone(culledPrompts[randomIndex])) < 7) {
+        randomIndex--;
+        if (randomIndex < 0) {
+          randomIndex += culledPrompts.length;
+        }
+      }
     }
   }
 }
